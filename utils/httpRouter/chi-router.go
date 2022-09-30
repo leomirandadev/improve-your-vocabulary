6a2f0539dfd52070b214164048e62378a5fe426f
leomiranda-tc <leonardo.miranda@tc.com.br>
2022-09-30 14:30:03 -0300

    fix: chi

--- utils/httpRouter/chi-router.go
+++ utils/httpRouter/chi-router.go
@@ -12,7 +12,7 @@ type chiRouter struct {
 }
 
 var (
-	muxDispatcher = chi.NewRouter()
+	chiDispatcher = chi.NewRouter()
 )
 
 func NewMuxRouter() Router {
@@ -20,26 +20,26 @@ func NewMuxRouter() Router {
 }
 
 func (*chiRouter) POST(uri string, f http.HandlerFunc) {
-	muxDispatcher.Post(uri, f)
+	chiDispatcher.Post(uri, f)
 }
 
 func (*chiRouter) GET(uri string, f http.HandlerFunc) {
-	muxDispatcher.Get(uri, f)
+	chiDispatcher.Get(uri, f)
 }
 
 func (*chiRouter) PUT(uri string, f http.HandlerFunc) {
-	muxDispatcher.Put(uri, f)
+	chiDispatcher.Put(uri, f)
 }
 
 func (*chiRouter) PATCH(uri string, f http.HandlerFunc) {
-	muxDispatcher.Patch(uri, f)
+	chiDispatcher.Patch(uri, f)
 }
 
 func (*chiRouter) DELETE(uri string, f http.HandlerFunc) {
-	muxDispatcher.Delete(uri, f)
+	chiDispatcher.Delete(uri, f)
 }
 
 func (m *chiRouter) SERVE(port string) {
 	fmt.Println("Online on http://localhost" + port)
-	log.Fatal(http.ListenAndServe(port, muxDispatcher))
+	log.Fatal(http.ListenAndServe(port, chiDispatcher))
 }
